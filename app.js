8d42f634558a439372ad394d07d3a5f09360dbf1
jackmonaco20 <jackmonaco20@yahoo.com>
2022-05-22 01:45:58 -0400

    pre

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+document.querySelector('button').addEventListener('click', getFetch)
 
+function getFetch(){
+    const choice = document.querySelector('input').value ,
+    const url = 'https://pokeapi.co/api/v2/pokemon'+choice
+
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+}
